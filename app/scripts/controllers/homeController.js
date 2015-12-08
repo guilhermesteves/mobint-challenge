03e879abd1fec2347825e10ea276edb3e33ff05a
Guilherme Esteves <guilherme.m.esteves@gmail.com>
2015-12-08 15:32:03 -0200

    add 2 methods fetch & fetchMore

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -15,14 +15,63 @@ angular.module('MobintChallenge')
     $scope.end      = false;
 
     $scope.fetchTopShows = function() {
+      TracktService.fetchTopShows(1)
+        .then(function(response) {
+
+          if (response.data) {
+
+            var shows = [];
+
+            for (var index in response.data) {
+
+              var responseItem = response.data[index];
+              var show = {
+                images: responseItem.images,
+                title: responseItem.title,
+                year: responseItem.year
+              };
+
+              shows.push(show);
+            }
+
+            $scope.topShows = $scope.topShows.concat(shows);
+
+            if (!response.data || response.data == []) {
+              $scope.end = true;
+            }
+          }
+
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
+
+    $scope.fetchMore = function() {
+
+      $scope.page = $scope.page+1;
+
       TracktService.fetchTopShows($scope.page)
         .then(function(response) {
 
           if (response.data) {
-            $scope.topShows.concat(response.data);
+
+            var shows = [];
+
+            for (var index in response.data) {
+
+              var responseItem = response.data[index];
+              var show = {
+                images: responseItem.images,
+                title: responseItem.title,
+                year: responseItem.year
+              };
+
+              shows.push(show);
+            }
+
+            $scope.topShows = $scope.topShows.concat(shows);
             $scope.page += 1;
 
-            if (response.data == []) {
+            if (!response.data || response.data == []) {
               $scope.end = true;
             }
           }
